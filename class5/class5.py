e4584c47268b4e840b73a053c5d363996455eaa5
abdulahad934 <adbulahad934@gmail.com>
2025-07-30 03:04:03 +0600

    added new class5 files

---
+++ class5/class5.py
@@ -0,0 +1,18 @@
+list_elements = [1, 2, 3, 4, 5, 6]
+
+index = 4
+value = 25
+
+
+
+list_elements[2:2] = [10, 20, 30]
+print(list_elements)
+
+
+
+
+
+
+for i in range(1, 10):
+    if 5<i:
+        print(i)
