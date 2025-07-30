db91de6ea6c8094f5078b00a270bba1a6f1e9d57
abdulahad934 <adbulahad934@gmail.com>
2025-07-30 21:18:37 +0600

    Update Even

--- class5/example.py
+++ class5/example.py
@@ -13,4 +13,25 @@ number = int(input("Enter Even or Odd Number cheker: "))
 
 result = Even_Odd_cheker(number)
 
-print(result)    
+print(result)
+
+
+
+def Even_Odd_Cheker(number):
+    Even = []
+    Odd = []
+    for i in number:
+        if i % 2 ==0:
+            Even +=  [i]
+            
+            print(f"Number is Even: {Even}")
+        else:
+            Odd += [i]
+            print(f"number is Odd: {Odd}")
+        
+    return number
+
+number = [1, 2, 3, 4, 5, 6, 6, 7]
+
+result = Even_Odd_Cheker(number)
+print(number)
